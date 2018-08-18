77b4ecf9c5fd2fb9f210f3776521b47894a89f1e
Jeffrey Mills <jeffreymills@Jeffreys-MacBook-Pro.local>
2018-08-18 15:20:35 -0400

    Completed Using MySql Tool

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+-- Burger Information for Tables --
+INSERT INTO burgers VALUES
+(NULL, 'Double Chili Cheddar Cheese Burger', false),
+(NULL, 'Mushroom and Bacon Burger with Provolone', false),
+(NULL, 'Classic Burger', false)
+
---
+++ server.js
@@ -0,0 +1,8 @@
+// ------------------- DEPENDENCIES ----------------//
+var express = require("express");
+var bodyParser = require("body-parser");
+var routes = require("./controllers/burgers_controller.js");
+var exphbs = require("express-handlebars");
+var app = express();
+// ------------------------------------------------- //
+
